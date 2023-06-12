02451e35eaac9cc42c986ed7839c482c72697f2d
rogerhh <rogerhh@umich.edu>
2023-06-12 11:31:31 -0700

    Added no riscv version

--- scripts/riscv/env.sh
+++ scripts/riscv/env.sh
@@ -6,11 +6,21 @@ set -o pipefail # Set pipe exit status to be the status of the last command befo
 SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 source $SCRIPT_DIR/color.sh
 
+# Use this to determing if installing RISCV
+export NO_RISCV=1
+
 export SCRIPT_PROMPT="${GREEN}${BOLD}[Kimera-VIO RISC-V Install]${NC}${UNBOLD}"
 export PROJECT_DIR="$(readlink -f ${SCRIPT_DIR}/../../..)"
 export INSTALL_DIR="${PROJECT_DIR}/local"
 export PATCH_DIR="${SCRIPT_DIR}/patch_files"
-export CMAKE_TOOLCHAIN_FILE="${SCRIPT_DIR}/riscv.cmake"
+
+if [ $NO_RISCV -eq 0 ]
+then
+    export CMAKE_TOOLCHAIN_FILE="${SCRIPT_DIR}/riscv.cmake"
+else
+    export CMAKE_TOOLCHAIN_FILE=""
+fi
+
 export CMAKE_COMMON_FLAGS="-DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DBUILD_SHARED_LIBS=OFF"
 export CHIPYARD_HOME="$(realpath "${PROJECT_DIR}")"
 
