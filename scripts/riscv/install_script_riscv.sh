e992b1e5c6d4eac8fd0e54cc07b224c363b4deb0
rogerhh <rogerhh@umich.edu>
2023-06-18 14:14:45 -0700

    Add breakpoints

--- scripts/riscv/install_script_riscv.sh
+++ scripts/riscv/install_script_riscv.sh
@@ -18,12 +18,62 @@ echo -e "${SCRIPT_PROMPT} Installing all dependencies to: ${INSTALL_DIR}"
 mkdir -p $INSTALL_DIR
 echo -e "${SCRIPT_PROMPT} Using Chipyard directory: ${CHIPYARD_HOME}"
 
-source ${SCRIPT_DIR}/install_boost_riscv.sh
+FROM_OPTIONS="boost gtsam opencv opengv dbow2 rpgo gflags glog kimera"
+COMPILE_INDEX=0
+
+FROM="${FROM:=boost}"
+
+for option in $FROM_OPTIONS
+do 
+    echo "$FROM $option"
+    if [[ $FROM == $option ]]
+    then
+        break
+    fi
+    ((COMPILE_INDEX=COMPILE_INDEX+1))
+done
+
+if [ $COMPILE_INDEX -le 0 ] 
+then
+    source ${SCRIPT_DIR}/install_boost_riscv.sh
+fi
+
+if [ $COMPILE_INDEX -le 1 ] 
+then
 source ${SCRIPT_DIR}/install_gtsam_riscv.sh
+fi
+
+if [ $COMPILE_INDEX -le 2 ] 
+then
 source ${SCRIPT_DIR}/install_opencv_riscv.sh
+fi
+
+if [ $COMPILE_INDEX -le 3 ] 
+then
 source ${SCRIPT_DIR}/install_opengv_riscv.sh
+fi
+
+if [ $COMPILE_INDEX -le 4 ] 
+then
 source ${SCRIPT_DIR}/install_DBoW2_riscv.sh
+fi
+
+if [ $COMPILE_INDEX -le 5 ] 
+then
 source ${SCRIPT_DIR}/install_Kimera-RPGO_riscv.sh
+fi
+
+if [ $COMPILE_INDEX -le 6 ] 
+then
 source ${SCRIPT_DIR}/install_gflags_riscv.sh
+fi
+
+if [ $COMPILE_INDEX -le 7 ] 
+then
 source ${SCRIPT_DIR}/install_glog_riscv.sh
+fi
+
+if [ $COMPILE_INDEX -le 8 ] 
+then
 source ${SCRIPT_DIR}/install_Kimera-VIO_riscv.sh
+fi
